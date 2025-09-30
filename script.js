88f7eac18f463e5d956a736920b8bce23f6d0f58
stacy8839 <stacym42@nycstudents.net>
2025-09-30 08:41:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Stacy M."; // update the part BETWEEN the "quotes"
+let userName = "stacy8839"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
